d6625ccb69132314872d7853e34ddb828c86b7d3
Ibrahim Michael O <segunmichael24@gmail.com>
2023-02-16 23:32:32 +0100

    SQL projects

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,3 +1,4 @@
--- A script that displays the number of records with id = 89
-SELECT COUNT('first_table')
-WHERE id=89;
+-- Displays the number of records with id = 89 in the table first_table in my MySQL server.
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
